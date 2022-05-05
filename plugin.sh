70feb3ffeef8601b335b5e9380242fb5a489395a
ArthurWang <yahui9119@gmail.com>
2022-05-05 23:36:16 +0800

    update

--- plugin.sh
+++ plugin.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 mkdir ~/.ssh
 chmod 700 ~/.ssh
-echo "${PLUGIN_GITOPS_SSHKEY}" | base64 -d > ~/.ssh/id_rsa
+echo "${GITOPS_SSHKEY}" | base64 -d > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-git config user.email ${PLUGIN_GITOPS_EMAIL}
-git config user.name ${PLUGIN_GITOPS_NAME}
-git clone ${PLUGIN_GITOPS_URL} GitOps --config core.sshCommand="ssh -i ~/.ssh/id_rsa"
-cd GitOps/${PLUGIN_GITOPS_DIRECTORY}
+git config user.email ${GITOPS_EMAIL}
+git config user.name ${GITOPS_NAME}
+git clone ${GITOPS_URL} GitOps --config core.sshCommand="ssh -i ~/.ssh/id_rsa"
+cd GitOps/${GITOPS_DIRECTORY}
 kustomize edit set image gcr.io/heptio-images/ks-guestbook-demo:0.1
 git add .
 git commit -m 'update'
