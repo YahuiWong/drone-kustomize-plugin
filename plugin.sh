032eec927811328ab32430b45fb54eef5dad0153
ArthurWang <yahui9119@gmail.com>
2022-05-06 21:58:35 +0800

    fix

--- plugin.sh
+++ plugin.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -xv
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 echo "${GITOPS_SSHKEY}" | base64 -d > ~/.ssh/id_rsa
@@ -6,10 +7,8 @@ chmod 600 ~/.ssh/id_rsa
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 git config user.email ${GITOPS_EMAIL}
 git config user.name ${GITOPS_NAME}
-set -xv
 git clone ${GITOPS_URL} GitOps --config core.sshCommand="ssh -i ~/.ssh/id_rsa"
 cd GitOps/${GITOPS_DIRECTORY}
-# ``kustomize edit set image gcr.io/heptio-images/ks-guestbook-demo:0.1``
 `${KUSTOMIZE_COMMAND}`
 git add .
 git commit -m "${GITOPS_COMMIT}"
